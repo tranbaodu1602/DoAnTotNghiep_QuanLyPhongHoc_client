4e78b9e97aac1442be9552eafa57591c113d6c39
vipker113 <79317962+vipker113@users.noreply.github.com>
2023-11-27 16:27:07 +0700

    nghi commit

--- client/src/page/GiangVien/FormYeuCau/FormYeuCau.tsx
+++ client/src/page/GiangVien/FormYeuCau/FormYeuCau.tsx
@@ -1,35 +1,66 @@
 import React, { useState } from 'react';
 import NavBar from '../../../components/NavBar/NavBar';
-import './FormYeuCau.scss'
-import Sider from '../../../components/Sider/Sider';
+import './FormYeuCau.scss';
 
+import { io, Socket } from 'socket.io-client';
+
+const socket: Socket = io('http://localhost:3001');
 
 const FormYeuCau: React.FC = () => {
     const storedData: any = localStorage.getItem('myDataKey');
     const data = JSON.parse(storedData);
 
+    socket.on('requestSchedule', (data: { GV: any }) => {
+        console.log(data.GV);
+    });
+
     const [formData, setFormData] = useState({
         hoTenGV: data.data.ThongTinCaNhan.hoTenGV,
         ngay: '',
-        mon: '',
-        tietDay: '',
-        lyDo: '',
+        monhoc: data.lich[0].tenMonHoc,
+        tietday: '',
+        lydo: '',
     });
 
-    const handleChange = (e) => {
+    const handleChange = (e: any) => {
         const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
+        if (e.target.type === 'select-one') {
+            const selectedIndex = e.target.selectedIndex;
+            const selectedValue = e.target.options[selectedIndex].value;
+            setFormData({
+                ...formData,
+                [name]: selectedValue,
+            });
+        } else {
+            setFormData({
+                ...formData,
+                [name]: value,
+            });
+        }
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
         console.log(formData);
-        // Thêm logic để lưu dữ liệu vào localStorage hoặc gửi đi đâu đó nếu cần
-    };
 
+        try {
+            const response = await fetch('http://localhost:3001/teacher/request-schedule', {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                console.log('Gửi yêu cầu thành công, vui lòng đợi xác nhận');
+            } else {
+                console.log('fail');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
 
     return (
         <>
@@ -38,42 +69,22 @@ const FormYeuCau: React.FC = () => {
                 <form onSubmit={handleSubmit}>
                     <label>
                         Tên:
-                        <input
-                            type="text"
-                            name="hoTenGV"
-                            value={formData.hoTenGV}
-                            readOnly
-                        />
+                        <input type="text" name="hoTenGV" value={formData.hoTenGV} readOnly />
                     </label>
 
                     <label>
                         Ngày:
-                        <input
-                            type="date"
-                            name="ngay"
-                            value={formData.ngay}
-                            onChange={handleChange}
-                        />
+                        <input type="date" name="ngay" value={formData.ngay} onChange={handleChange} />
                     </label>
 
                     <label>
                         Tiết dạy:
-                        <input
-                            type="text"
-                            name="tietDay"
-                            value={formData.tietDay}
-                            onChange={handleChange}
-                        />
+                        <input type="text" name="tietday" value={formData.tietday} onChange={handleChange} />
                     </label>
                     <label>
                         Môn:
-                        <select
-                            name="mon"
-                            value={formData.mon}
-                            onChange={handleChange}
-                        >
-
-                            {data.lich.map(monHoc => (
+                        <select name="monhoc" value={formData.monhoc} onChange={handleChange}>
+                            {data.lich.map((monHoc: any) => (
                                 <option key={monHoc.maLopHocPhan} value={monHoc.tenMonHoc}>
                                     {monHoc.tenMonHoc}
                                 </option>
@@ -83,11 +94,7 @@ const FormYeuCau: React.FC = () => {
 
                     <label>
                         Lý do:
-                        <textarea
-                            name="lyDo"
-                            value={formData.lyDo}
-                            onChange={handleChange}
-                        />
+                        <textarea name="lydo" value={formData.lydo} onChange={handleChange} />
                     </label>
 
                     <button type="submit" className="submit-button">
@@ -96,7 +103,7 @@ const FormYeuCau: React.FC = () => {
                 </form>
             </div>
         </>
-    )
+    );
 };
 
 export default FormYeuCau;
