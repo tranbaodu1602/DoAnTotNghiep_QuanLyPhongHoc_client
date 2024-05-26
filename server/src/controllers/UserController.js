ed8d5aa0a5cc527f6ba057e4801a73307a45880c
vipker113 <79317962+vipker113@users.noreply.github.com>
2024-05-26 08:03:19 +0700

    final commit

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,6 +1,6 @@
 const userService = require('../services/UserService');
 const bcrypt = require('bcrypt');
-const jwt = require("../configs/jwt");
+const jwt = require('../configs/jwt');
 
 const createUser = async (req, res) => {
     try {
@@ -25,6 +25,40 @@ const changePassword = async (req, res) => {
 };
 
 const userLogin = async (req, res) => {
+    // try {
+    //     const { username, password } = req.body;
+    //     if (!username || !password) {
+    //         return res.status(200).json({
+    //             status: 'ERR',
+    //             message: 'The input is required',
+    //         });
+    //     }
+
+    //     const data = await userService.userLogin(req.body);
+    //     console.log("data:" , data);
+    //     if(data.data.status !== 'fail') {
+    //          // Giả định rằng userService.userLogin trả về thông tin người dùng nếu đăng nhập thành công
+    //         // Tạo payload cho JWT
+    //         // const payload = { id: data.id, username: data.username };
+
+    //         // Tạo token với thời gian hết hạn là 7 ngày
+    //         console.log("test" + data.data.checkUser.loaitaikhoan);
+    //         const token = jwt.generateToken(
+    //             { user_id : data.data.checkUser._id , role : data.data.checkUser.loaitaikhoan},
+    //             "7d"
+    //           );
+    //           return res.status(200).json({
+    //             status: 'OK',
+    //             message: 'Login successful',
+    //             dataLogin: data,
+    //             token: token,
+    //         });
+    //     } else {
+    //         return res.status(200).json({
+    //             status: 'ERR',
+    //             message: 'Invalid username or password',
+    //         });
+    //     }
     try {
         const { username, password } = req.body;
         if (!username || !password) {
@@ -35,33 +69,10 @@ const userLogin = async (req, res) => {
         }
 
         const data = await userService.userLogin(req.body);
-        console.log("data:" , data);
-        if(data.data.status !== 'fail') {
-             // Giả định rằng userService.userLogin trả về thông tin người dùng nếu đăng nhập thành công
-            // Tạo payload cho JWT
-            // const payload = { id: data.id, username: data.username };
 
-            // Tạo token với thời gian hết hạn là 7 ngày
-            console.log("test" + data.data.checkUser.loaitaikhoan);
-            const token = jwt.generateToken(
-                { user_id : data.data.checkUser._id , role : data.data.checkUser.loaitaikhoan},
-                "7d"
-              );            
-              return res.status(200).json({
-                status: 'OK',
-                message: 'Login successful',
-                dataLogin: data,
-                token: token,
-            });
-        } else {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'Invalid username or password',
-            });
-        }
-        // return res.status(200).json({
-        //     dataLogin: data,
-        // });
+        return res.status(200).json({
+            dataLogin: data,
+        });
     } catch (e) {
         throw e;
     }
